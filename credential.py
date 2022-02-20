3163c6b85f91d6945b98293cde98905b8fe4397b
Bett-C <collins.kkiprotich.student@moringaschool.com>
2022-02-20 03:08:20 +0300

    it will add a method that save credentials

--- credential.py
+++ credential.py
@@ -5,7 +5,7 @@ class Credentials:
     this class will enable the generation of instance classes
     '''
   
-def _init_ (seld,credential_name,user_password,credential_password):
+def _init_ (self,credential_name,user_password,credential_password):
       
     '''
     _init_ method will define the properties of the User objects
@@ -16,5 +16,16 @@ def _init_ (seld,credential_name,user_password,credential_password):
      self.credential_password   = credential_password
      
      '''
+    self.user_password   = user_password
+    self.credential_name   = credential_name
+    self.credential_password   = credential_password
      
-    
+    def save_credential(self):
+        
+        '''
+        this is a methof that will save user's credentials
+        '''
+        
+        Credentials.credential_list.append(self)
+        
+        
