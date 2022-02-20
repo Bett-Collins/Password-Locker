df6276b70b40e0a6ea6150eefab39add0f9c6de8
Bett-C <collins.kkiprotich.student@moringaschool.com>
2022-02-20 03:01:19 +0300

    it will create class and init method to define properties

---
+++ credential.py
@@ -0,0 +1,20 @@
+from user import User
+
+class Credentials:
+    '''
+    this class will enable the generation of instance classes
+    '''
+  
+def _init_ (seld,credential_name,user_password,credential_password):
+      
+    '''
+    _init_ method will define the properties of the User objects
+     
+     args:
+     self.user_password   = user_password
+     self.credential_name   = credential_name
+     self.credential_password   = credential_password
+     
+     '''
+     
+    
